dad3350291fab5aef7899772bfc318a0b81dedfa
Marcos Lara <marcoslaraperez92@gmail.com>
2026-05-17 17:34:20 +0200

    Refactor

--- src/test/java/com/devmark/cloneuber/CloneUberApplicationTests.java
+++ src/test/java/com/devmark/cloneuber/CloneUberApplicationTests.java
@@ -1,13 +1,11 @@
 package com.devmark.cloneuber;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CloneUberApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
